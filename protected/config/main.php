68bc0530662330831142cbfb515271f02a5f91d9
Mihail Marchitan <markitanm@gmail.com>
2012-10-08 11:01:17 +0300

    Update protected/config/main.php
    Обновление языка

--- protected/config/main.php
+++ protected/config/main.php
@@ -8,7 +8,7 @@
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
 	'name'=>'Новостной портал',
-        'sourceLanguage' => 'en_US',
+        //'sourceLanguage' => 'en_US',
         'language' => 'ru',
         // preloading 'log' component
 	'preload'=>array('log'),
